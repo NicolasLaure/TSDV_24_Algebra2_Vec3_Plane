ef6a1b51ae3e1486faea103b564d4f626dd4890e
Nicodragon25 <nicolasariellaure@gmail.com>
2024-04-28 20:10:09 -0300

    Add private result vector variable

--- Assets/Scripts/Ejercicios/EjerciciosVector3.cs
+++ Assets/Scripts/Ejercicios/EjerciciosVector3.cs
@@ -7,11 +7,18 @@ namespace ActividadEjercicios
 {
     public class EjerciciosVector3 : MonoBehaviour
     {
+        #region Variables
+
         #region UserInputs
         [SerializeField] private Ejercicios selectedExercise;
         [SerializeField] private Color VectorColor = Color.red;
         [SerializeField] private Vec3 A;
         [SerializeField] private Vec3 B;
         #endregion
+
+        private Vec3 resultVector;
+
+        #endregion
+
     }
 }
