79b096c20aee2320546bd5d591fb5f21f0893463
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-09 10:36:55 -0300

    Remove duplicated self plane script

--- Assets/Scripts/Plane/Self_Plane.cs
+++ Assets/Scripts/Plane/Self_Plane.cs
@@ -27,27 +27,22 @@ namespace CustomMath
         public static Self_Plane Translate(Self_Plane plane, Vec3 translation)
         {
             throw new NotImplementedException();
-
         }
         public Vec3 ClosestPointOnPlane(Vec3 point)
         {
             throw new NotImplementedException();
-
         }
         public void Flip()
         {
             throw new NotImplementedException();
-
         }
         public float GetDistanceToPoint(Vec3 point)
         {
             throw new NotImplementedException();
-
         }
         public bool GetSide(Vec3 point)
         {
             throw new NotImplementedException();
-
         }
         public bool SameSide(Vec3 inPt0, Vec3 inPt1)
         {
@@ -56,17 +51,14 @@ namespace CustomMath
         public void Set3Points(Vec3 a, Vec3 b, Vec3 c)
         {
             throw new NotImplementedException();
-
         }
         public void SetNormalAndPosition(Vec3 inNormal, Vec3 inPoint)
         {
             throw new NotImplementedException();
-
         }
         public void Translate(Vec3 translation)
         {
             throw new NotImplementedException();
-
         }
     }
 }
